0e549aac9b3ce2caac9589084bcd840218aa8a3b
Iraida22 <iraida.iva22@mail.ru>
2022-05-04 16:08:37 +0300

    refactor: apply webpack, eslint, add index.html, index.js

---
+++ webpack.config.js
@@ -0,0 +1,93 @@
+const path = require("path");
+
+const buildPath = path.resolve(__dirname, "dist");
+const srcPath = path.resolve(__dirname, "src");
+const contentPath = path.resolve(__dirname, "public");
+
+const HtmlWebpackPlugin = require("html-webpack-plugin");
+const MiniCssExtractPlugin = require("mini-css-extract-plugin");
+const ESLintPlugin = require("eslint-webpack-plugin");
+
+const isProd = process.env.NODE_ENV === "production";
+const getSettingsForStyles = (withModules = false) => {
+    return [
+        MiniCssExtractPlugin.loader,
+        !withModules
+            ? "css-loader"
+            : {
+                loader: "css-loader",
+                options: {
+                    modules: {
+                        localIdentName: !isProd
+                            ? "[path][name]__[local]"
+                            : "[hash:base64]",
+                    },
+                },
+            },
+        {
+            loader: "postcss-loader",
+            options: {
+                postcssOptions: {
+                    plugins: ["autoprefixer"],
+                },
+            },
+        },
+        "sass-loader",
+    ];
+};
+
+module.exports = {
+    entry: path.join(srcPath, "index.js"),
+    target: !isProd ? "web" : "browserslist",
+    devtool: !isProd ? "hidden-source-map" : "eval-source-map",
+    output: {
+        path: buildPath,
+        filename: "bundle.js",
+        publicPath: "/",
+    },
+    plugins: [
+        new HtmlWebpackPlugin({
+            template: path.join(contentPath, "index.html"),
+        }),
+        new MiniCssExtractPlugin({
+            filename: "[name]-[hash].css",
+        }),
+        new ESLintPlugin()
+    ],
+    module: {
+        rules: [
+            {
+                test: /\.module\.s?css$/,
+                use: getSettingsForStyles(true),
+            },
+            {
+                test: /\.s?css$/,
+                exclude: /\.module\.s?css$/,
+                use: getSettingsForStyles(),
+            },
+            {
+                test: /\.(png | svg | jpg)$/,
+                use: "asset/resource",
+            },
+        ],
+    },
+    resolve: {
+        extensions: [".js"],
+        alias: {
+            components: path.join(srcPath, "components"),
+            config: path.join(srcPath, "config"),
+            styles: path.join(srcPath, "styles"),
+            utils: path.join(srcPath, "utils"),
+            store: path.join(srcPath, "store"),
+            shared: path.join(srcPath, "shared"),
+            root: path.join(srcPath, "root"),
+            App: path.join(srcPath, "App"),
+        },
+    },
+    devServer: {
+        host: "127.0.0.1",
+        port: 9000,
+        hot: true,
+        historyApiFallback: true,
+    }
+};
