197f2ef7771ce5e1111a6169ff1de0fd0175124b
Iraida22 <iraida.iva22@mail.ru>
2022-05-09 15:59:01 +0300

    feat: add object 'keys'

---
+++ src/utils/keys.js
@@ -0,0 +1,242 @@
+export const keys = {
+  Digit1: {
+    code: 'Digit1',
+    charRu: '1',
+    charEn: '1',
+  },
+  Digit2: {
+    code: 'Digit2',
+    charRu: '2',
+    charEn: '2',
+  },
+  Digit3: {
+    code: 'Digit3',
+    charRu: '3',
+    charEn: '3',
+  },
+  Digit4: {
+    code: 'Digit4',
+    charRu: '4',
+    charEn: '4',
+  },
+  Digit5: {
+    code: 'Digit5',
+    charRu: '5',
+    charEn: '5',
+  },
+  Digit6: {
+    code: 'Digit6',
+    charRu: '6',
+    charEn: '6',
+  },
+  Digit7: {
+    code: 'Digit7',
+    charRu: '7',
+    charEn: '7',
+  },
+  Digit8: {
+    code: 'Digit8',
+    charRu: '8',
+    charEn: '8',
+  },
+  Digit9: {
+    code: 'Digit9',
+    charRu: '9',
+    charEn: '9',
+  },
+  Digit0: {
+    code: 'Digit0',
+    charRu: '8',
+    charEn: '8',
+  },
+  Backquote: {
+    code: 'Backquote',
+    charRu: 'ё',
+    charEn: '`',
+  },
+  NumpadSubtract: {
+    code: 'NumpadSubtract',
+    charRu: '-',
+    charEn: '-',
+  },
+  Equal: {
+    code: 'Equal',
+    charRu: '=',
+    charEn: '=',
+  },
+  KeyQ: {
+    code: 'KeyQ',
+    charRu: 'й',
+    charEn: 'q',
+  },
+  KeyW: {
+    code: 'KeyW',
+    charRu: 'ц',
+    charEn: 'w',
+  },
+  KeyE: {
+    code: 'KeyE',
+    charRu: 'у',
+    charEn: 'e',
+  },
+  KeyR: {
+    code: 'KeyR',
+    charRu: 'к',
+    charEn: 'r',
+  },
+  KeyT: {
+    code: 'KeyT',
+    charRu: 'е',
+    charEn: 't',
+  },
+  KeyY: {
+    code: 'KeyY',
+    charRu: 'н',
+    charEn: 'y',
+  },
+  KeyU: {
+    code: 'KeyU',
+    charRu: 'г',
+    charEn: 'u',
+  },
+  KeyI: {
+    code: 'KeyI',
+    charRu: 'ш',
+    charEn: 'i',
+  },
+  KeyO: {
+    code: 'KeyO',
+    charRu: 'щ',
+    charEn: 'o',
+  },
+  KeyP: {
+    code: 'KeyP',
+    charRu: 'з',
+    charEn: 'p',
+  },
+  BracketLeft: {
+    code: 'BracketLeft',
+    charRu: 'х',
+    charEn: '[',
+  },
+  BracketRight: {
+    code: 'BracketRight',
+    charRu: 'ъ',
+    charEn: ']',
+  },
+  KeyA: {
+    code: 'KeyA',
+    charRu: 'ф',
+    charEn: 'a',
+  },
+  KeyS: {
+    code: 'KeyS',
+    charRu: 'ы',
+    charEn: 's',
+  },
+  KeyD: {
+    code: 'KeyD',
+    charRu: 'в',
+    charEn: 'd',
+  },
+  KeyF: {
+    code: 'KeyF',
+    charRu: 'а',
+    charEn: 'f',
+  },
+  KeyG: {
+    code: 'KeyG',
+    charRu: 'п',
+    charEn: 'g',
+  },
+  KeyH: {
+    code: 'KeyH',
+    charRu: 'р',
+    charEn: 'h',
+  },
+  KeyJ: {
+    code: 'KeyJ',
+    charRu: 'о',
+    charEn: 'j',
+  },
+  KeyK: {
+    code: 'KeyK',
+    charRu: 'л',
+    charEn: 'k',
+  },
+  KeyL: {
+    code: 'KeyL',
+    charRu: 'д',
+    charEn: 'l',
+  },
+  Semicolon: {
+    code: 'Semicolon',
+    charRu: 'ж',
+    charEn: ';',
+  },
+  Quote: {
+    code: 'Quote',
+    charRu: 'э',
+    charEn: '\'',
+  },
+  KeyZ: {
+    code: 'KeyZ',
+    charRu: 'я',
+    charEn: 'z',
+  },
+  KeyX: {
+    code: 'KeyX',
+    charRu: 'ч',
+    charEn: 'x',
+  },
+  KeyC: {
+    code: 'KeyC',
+    charRu: 'с',
+    charEn: 'c',
+  },
+  KeyV: {
+    code: 'KeyV',
+    charRu: 'м',
+    charEn: 'v',
+  },
+  KeyB: {
+    code: 'KeyB',
+    charRu: 'и',
+    charEn: 'b',
+  },
+  KeyN: {
+    code: 'KeyN',
+    charRu: 'т',
+    charEn: 'n',
+  },
+  KeyM: {
+    code: 'KeyM',
+    charRu: 'ь',
+    charEn: 'm',
+  },
+  NumpadDecimal: {
+    code: 'NumpadDecimal',
+    charRu: 'б',
+    charEn: ',',
+  },
+  Period: {
+    code: 'Period',
+    charRu: 'ю',
+    charEn: '.',
+  },
+  Slash: {
+    code: 'Slash',
+    charRu: '.',
+    charEn: '/',
+  },
+  Space: {
+    code: 'Space',
+    charRu: ' ',
+    charEn: ' ',
+  },
+  Backslash: {
+    code: 'Backslash',
+    charRu: '\\',
+    charEn: '\\',
+  },
+};
